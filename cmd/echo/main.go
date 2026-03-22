1f18cded86cfc6856a40403cdd418a99318cf223
Maxim <cyberhunt@MacBook-Air-Maxim.local>
2026-03-22 22:06:01 +0300

    add echo comand for terminal

--- cmd/echo/main.go
+++ cmd/echo/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"os"
+)
+
+var dislayReturn = true
 
 func main() {
-	fmt.Println("echo util")
+	args := os.Args[1:]
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+
+	if len(args) == 0 {
+		w.WriteString("\n")
+		return
+	}
+	if args[0] == "-n" {
+		dislayReturn = false
+		args = append(args[:0], args[1:]...)
+	}
+	for i, val := range args {
+		if i > 0 {
+			w.WriteString(" ")
+		}
+		w.WriteString(val)
+	}
+	if dislayReturn {
+		w.WriteString("\n")
+	}
 }
